47dd2d91a7ea5c360a3e85d050abd81850c5eb1c
Yurchuk 0leksiy <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>
2022-08-21 12:48:56 +0300

    firstcomm

---
+++ HomeWork9/HomeWork9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число");
+        double y = double.Parse(Console.ReadLine());
+        Console.WriteLine("Виберіть яку дію ви хочете обрати");
+        Console.WriteLine("+;-;*;/;");
+        string z = Console.ReadLine();
+
+
+
+        switch (z)
+        {
+            case "+":
+                Console.WriteLine(x+y);
+                break;
+
+            case "-":
+                Console.WriteLine(x - y);
+                break;
+            case "*":
+                Console.WriteLine(x * y);
+                break;
+            case "/":
+               try
+                    {
+                        double q = x / y;
+                    Console.WriteLine(q);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Возникло исключение DivideByZeroException");
+
+                    }
+                break;
+        }
+    }
+}
+
